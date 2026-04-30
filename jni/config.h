6cce935fa0da2ab67236cb7b046b00f7f488aa2e
brruham-arch <email_kamu@gmail.com>
2026-04-30 18:37:04 +0700

    fix: config.h as file, no heredoc in yaml

---
+++ jni/config.h
@@ -0,0 +1,6 @@
+#define PACKAGE_VERSION "1.51"
+#define USE_ASYNC 0
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define PATH_ESPEAK_DATA "/storage/emulated/0/espeak-ng-data"
