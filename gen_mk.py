d032d26b4b8e4eb7987e349b1dfa139c7934a552
brruham-arch <email_kamu@gmail.com>
2026-04-30 19:00:08 +0700

    fix: patch ucd include path + fix gen_mk includes

--- gen_mk.py
+++ gen_mk.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python3
-import os, glob
+import os
 
-# Kumpulkan semua .c dari espeak-ng/src (semua subfolder)
-srcs = []
 skip_dirs = {'tests', 'windows', 'include'}
+
+# Source files
+srcs = []
 for root, dirs, files in os.walk("jni/espeak-ng/src"):
     dirs[:] = [d for d in sorted(dirs) if d not in skip_dirs]
     for f in sorted(files):
         if f.endswith('.c'):
-            srcs.append(os.path.join(root, f))
+            srcs.append(os.path.join(root, f).replace("jni/espeak-ng/src/", "espeak-ng/src/"))
 
-srcs_rel = [s.replace("jni/espeak-ng/src/", "espeak-ng/src/") for s in srcs]
-srcs_str = " \\\n    ".join(srcs_rel)
-print(f"Found {len(srcs_rel)} source files")
+srcs_str = " \\\n    ".join(srcs)
+print(f"Found {len(srcs)} source files")
 
-# Kumpulkan semua subfolder dari src/ untuk include path
-include_dirs = set()
-include_dirs.add("$(LOCAL_PATH)/espeak-ng/src")
-include_dirs.add("$(LOCAL_PATH)/espeak-ng/src/include")
-include_dirs.add("$(LOCAL_PATH)/espeak-ng/src/include/espeak-ng")
-include_dirs.add("$(LOCAL_PATH)/espeak-ng/include")
-include_dirs.add("$(LOCAL_PATH)/espeak-ng/include/espeak-ng")
+# Include paths - eksplisit semua yang dibutuhkan
+# <ucd/ucd.h> butuh parent dari folder ucd = src/libespeak-ng atau src
+# Kita tambah semua subfolder langsung
+inc = [
+    "$(LOCAL_PATH)/espeak-ng/src",
+    "$(LOCAL_PATH)/espeak-ng/src/libespeak-ng",  # parent dari ucd include
+    "$(LOCAL_PATH)/espeak-ng/src/ucd",
+    "$(LOCAL_PATH)/espeak-ng/src/compat",
+    "$(LOCAL_PATH)/espeak-ng/src/include",
+    "$(LOCAL_PATH)/espeak-ng/src/include/espeak-ng",
+    "$(LOCAL_PATH)/espeak-ng/src/include/espeak",
+    "$(LOCAL_PATH)/espeak-ng/include",
+    "$(LOCAL_PATH)/espeak-ng/include/espeak-ng",
+    "$(LOCAL_PATH)/espeak-ng/include/espeak",
+]
+# Tambah semua subfolder lainnya secara dinamis
 for root, dirs, files in os.walk("jni/espeak-ng/src"):
     dirs[:] = [d for d in sorted(dirs) if d not in skip_dirs]
     rel = root.replace("jni/espeak-ng/src", "$(LOCAL_PATH)/espeak-ng/src")
-    include_dirs.add(rel)
+    if rel not in inc:
+        inc.append(rel)
 
-includes_str = " \\\n    ".join(sorted(include_dirs))
+inc_str = " \\\n    ".join(inc)
 
-# Cari header speak_lib.h dan fix include di voicetts.cpp
+# Fix include di voicetts.cpp
 for root, dirs, files in os.walk("jni/espeak-ng"):
     for f in files:
         if f == "speak_lib.h":
             hrel = root.replace("jni/", "")
             with open("jni/voicetts.cpp", "r") as fp:
-                content = fp.read()
+                c = fp.read()
             for old in ['espeak-ng/speak_lib.h', 'espeak/speak_lib.h']:
-                content = content.replace(f'#include "{old}"', f'#include "{hrel}/speak_lib.h"')
+                c = c.replace(f'#include "{old}"', f'#include "{hrel}/speak_lib.h"')
             with open("jni/voicetts.cpp", "w") as fp:
-                fp.write(content)
+                fp.write(c)
             print(f"Fixed include -> {hrel}/speak_lib.h")
             break
 
@@ -47,7 +57,7 @@ mk = f"""LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := espeak-ng
 LOCAL_SRC_FILES := {srcs_str}
-LOCAL_C_INCLUDES := {includes_str}
+LOCAL_C_INCLUDES := {inc_str}
 LOCAL_CFLAGS    := -O2 -fPIC -DANDROID -DUSE_ASYNC=0 -DLIBESPEAK_NG_EXPORT="" \\
     -Wno-error -Wno-implicit-function-declaration -Wno-incompatible-pointer-types
 include $(BUILD_STATIC_LIBRARY)
@@ -55,7 +65,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := voicetts
 LOCAL_SRC_FILES := voicetts.cpp
-LOCAL_C_INCLUDES := {includes_str}
+LOCAL_C_INCLUDES := {inc_str}
 LOCAL_STATIC_LIBRARIES := espeak-ng
 LOCAL_LDLIBS    := -llog -landroid -ldl -lm
 LOCAL_CPPFLAGS  := -std=c++17 -O2 -fPIC -fvisibility=hidden -DANDROID
@@ -64,5 +74,4 @@ include $(BUILD_SHARED_LIBRARY)
 
 with open("jni/Android.mk", "w") as f:
     f.write(mk)
-
 print("Android.mk generated OK")
